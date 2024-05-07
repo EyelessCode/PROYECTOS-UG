82c6666081f47243a2259d31afae642ad2530301
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-05-06 21:26:33 -0500

    Se creó repositorios, Se creó la clase donde se ejecutará la app y se agregó el FXML.
    Se realizó modificaciones dentro del Scene Builder, básicamente estuve jugueteando para poder recordar. Por el momento sólo tiene de algunos componentes un par de IDs. Se eliminó la clase "App" y creo que nada más.

---
+++ UG-00-Interface_utilizando_Layouts/src/vista/InterfaceLayout.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.RowConstraints;
+
+public class InterfaceLayout {
+
+    @FXML
+    private RowConstraints gpPrimero;
+
+    @FXML
+    private RowConstraints gpSegundo;
+
+}
