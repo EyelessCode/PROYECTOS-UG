2ff969dd96f4ca3fe3c14c6cf8d2e2194a7d3481
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-06-16 18:39:04 -0500

    Aplicación de los botones en la clase Interfaz.
    Se agregó los botones y las etiquetas en la clase Interfaz.

--- UG-01-FormularioJavaFX/src/ug/interfaz/Interfaz.java
+++ UG-01-FormularioJavaFX/src/ug/interfaz/Interfaz.java
@@ -1,5 +1,45 @@
 package ug.interfaz;
 
-public class Interfaz {
-    
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+
+public class Interfaz extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Label lb=new Label("Botón:");
+        Button btn=new Button("Botón");
+        Label lb1=new Label("HiperVínculo:");
+        Hyperlink hLink=new Hyperlink("HiperVínculo");
+        Label lb2=new Label("Botón de activación:");
+        ToggleButton tbtn=new ToggleButton("Botón de activación");
+        Label lb3=new Label("Boton de Radio:");
+        RadioButton rbtn=new RadioButton("Boton de Radio");
+        Label lb4=new Label("Campo de Texto:");
+        TextField txf=new TextField("Algo de texto...");
+        Label lb5=new Label("Campo de Contraseña:");
+        PasswordField psswf=new PasswordField();
+        Label lb6=new Label("Área de texto:");
+        TextArea txa=new TextArea("Este es un texto muy largo, solo envolverá varias líneas.");
+        Label lb7=new Label("Indicador de Progreso:");
+        ProgressIndicator pi=new ProgressIndicator();
+        Label lb8=new Label("Barra de Progreso:");
+        ProgressBar pb=new ProgressBar();
+        Label lb9=new Label("Deslizador:");
+        Slider sl=new Slider();
+
+
+    }
+
 }
