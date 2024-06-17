4ec5b53ff651f649139e7fbef2ce79c645a6bfa2
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-06-16 21:45:12 -0500

    Actualización clase InterfazMenu.
    Se agregó le método Start y los menús.

--- UG-02-SistemaMenus/src/ug/interfaz/InterfazMenu.java
+++ UG-02-SistemaMenus/src/ug/interfaz/InterfazMenu.java
@@ -1,5 +1,23 @@
 package ug.interfaz;
 
-public class InterfazMenu {
+import javafx.application.Application;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class InterfazMenu extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        MenuBar menuBar=new MenuBar();
+
+        Menu menuArchivo=new Menu("ARCHIVO");
+        Menu menuEditar=new Menu("EDITAR");
+        Menu menuAyuda=new Menu("AYUDA");
+        menuBar.getMenus().addAll(menuArchivo,menuEditar,menuAyuda);
+
+        MenuItem archivoNuevo=new MenuItem("NUEVO");
+    }
 
 }
