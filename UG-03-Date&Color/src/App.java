cd66b09e1f07701719b3295748aa5fc010bd8474
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-06-17 18:09:49 -0500

    Llamada de App a Interfaz.
    Se estableció la llamada del Main a la clase Interfaz.

--- UG-03-Date&Color/src/App.java
+++ UG-03-Date&Color/src/App.java
@@ -1,5 +1,7 @@
+import ug.interfaz.Interfaz;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Interfaz.launch(Interfaz.class,args);
     }
 }
