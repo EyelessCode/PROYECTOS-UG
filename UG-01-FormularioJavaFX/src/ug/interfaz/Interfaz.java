df0c24772a1dbfde3439e87769c163d1cacb419b
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-06-16 19:04:17 -0500

    Se agregó GridPane al Interfaz.
    Agregación del GridPane y ajustes de las posiciones.

--- UG-01-FormularioJavaFX/src/ug/interfaz/Interfaz.java
+++ UG-01-FormularioJavaFX/src/ug/interfaz/Interfaz.java
@@ -1,7 +1,9 @@
 package ug.interfaz;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -12,6 +14,8 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Interfaz extends Application{
@@ -20,12 +24,19 @@ public class Interfaz extends Application{
     public void start(Stage arg0) throws Exception {
         Label lb=new Label("Botón:");
         Button btn=new Button("Botón");
+        Label lb0=new Label("Comprobador de Caja:");
+        CheckBox cb=new CheckBox("Comprobador de Caja");
         Label lb1=new Label("HiperVínculo:");
         Hyperlink hLink=new Hyperlink("HiperVínculo");
         Label lb2=new Label("Botón de activación:");
         ToggleButton tbtn=new ToggleButton("Botón de activación");
+        ToggleGroup grupo=new ToggleGroup();
+        tbtn.setToggleGroup(grupo);
+
         Label lb3=new Label("Boton de Radio:");
         RadioButton rbtn=new RadioButton("Boton de Radio");
+        Label lb4_0=new Label("Etiqueta:");
+        Label lb4_1=new Label("Etiqueta");
         Label lb4=new Label("Campo de Texto:");
         TextField txf=new TextField("Algo de texto...");
         Label lb5=new Label("Campo de Contraseña:");
@@ -39,7 +50,35 @@ public class Interfaz extends Application{
         Label lb9=new Label("Deslizador:");
         Slider sl=new Slider();
 
+        GridPane gp=new GridPane();
+        gp.setHgap(10);
+        gp.setVgap(10);
+        gp.setAlignment(Pos.TOP_LEFT);
 
+        gp.add(lb, 0, 0);
+        gp.add(btn, 1, 0);
+        gp.add(lb0, 0, 1);
+        gp.add(cb, 1, 1);
+        gp.add(lb1, 0, 2);
+        gp.add(hLink, 1, 2);
+        gp.add(lb2, 0, 3);
+        gp.add(tbtn, 1, 3);
+        gp.add(lb3, 0, 4);
+        gp.add(rbtn, 1, 4);
+        gp.add(lb4_0, 0, 5);
+        gp.add(lb4_1, 1, 5);
+        gp.add(lb4, 0, 6);
+        gp.add(txf, 1, 6);
+        gp.add(lb5, 0, 7);
+        gp.add(psswf, 1, 7);
+        gp.add(lb6, 0, 8);
+        gp.add(txa, 1, 8);
+        gp.add(lb7, 0, 9);
+        gp.add(pi, 1, 9);
+        gp.add(lb8, 0, 10);
+        gp.add(pb, 1, 10);
+        gp.add(lb9, 0, 11);
+        gp.add(sl, 1, 11);
     }
 
 }
