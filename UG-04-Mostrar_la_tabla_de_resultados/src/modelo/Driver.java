604d28c5ccc366c1490e4031e13a0a67f1ff05f8
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-06 15:21:31 -0500

    Creación de la entidad/modelo Driver.
    Creación de la entidad/modelo Driver.

---
+++ UG-04-Mostrar_la_tabla_de_resultados/src/modelo/Driver.java
@@ -0,0 +1,99 @@
+package modelo;
+
+public class Driver {
+    private int driverId;
+    private String driverRef;
+    private int number; 
+    private String code;
+    private String forename;
+    private String surname;
+    private String dob;
+    private String nationality;
+    private String url;
+    
+    public Driver(int driverId, String driverRef, int number, String code, String forename, String surname, String dob,
+            String nationality, String url) {
+        this.driverId = driverId;
+        this.driverRef = driverRef;
+        this.number = number;
+        this.code = code;
+        this.forename = forename;
+        this.surname = surname;
+        this.dob = dob;
+        this.nationality = nationality;
+        this.url = url;
+    }
+
+    public int getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(int driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getDriverRef() {
+        return driverRef;
+    }
+
+    public void setDriverRef(String driverRef) {
+        this.driverRef = driverRef;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
