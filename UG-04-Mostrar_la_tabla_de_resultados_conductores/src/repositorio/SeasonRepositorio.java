1b864a09aaf730300a1d2c46d07a91fd45aff76b
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-08 18:00:27 -0500

    Creación de un nuevo repositorio: SeasonRepositorio.
    Creación de SeasonRepositorio, se creó la url, usuario y contraseña. Se creó el método lista y la lista par listar los campos.

---
+++ UG-04-Mostrar_la_tabla_de_resultados_conductores/src/repositorio/SeasonRepositorio.java
@@ -0,0 +1,17 @@
+package repositorio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modelo.Season;
+
+public class SeasonRepositorio {
+    String url="jdbc:mysql//127.0.0.1:3306/formula1";
+    String usuario="cristhian";
+    String contrasenia="cris03022";
+
+    public List<Season> anioGeneral(){
+        List<Season> listSeasons=new ArrayList<Season>();
+        return null;
+    }
+}
