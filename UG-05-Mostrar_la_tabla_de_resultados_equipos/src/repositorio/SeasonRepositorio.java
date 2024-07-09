6ddf579c9d2032aa79287d42d2278c33fa796ec1
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 13:22:22 -0500

    Actualización en SeasonRepositorio.
    Agregación de un Try-Catch, donde está al conexión, consulta y declaración. Creación de un While para el recorrido de los campos de la tabla y una excepción. Retorna la lista.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/SeasonRepositorio.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/SeasonRepositorio.java
@@ -1,5 +1,9 @@
 package repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +17,25 @@ public class SeasonRepositorio {
     public List<Season> seasonAll(){
         List<Season> seasonList=new ArrayList<Season>();
 
+        try {
+            Connection cnt=DriverManager.getConnection(url, user, password);
+
+            String sql="SELECT * FROM `seasons`;";
+            Statement st=cnt.createStatement();
+            ResultSet rs=st.executeQuery(sql);
+
+            while (rs.next()) {
+                int year=rs.getInt("year");
+                String url=rs.getString("url");
+
+                Season s=new Season(year, url);
+
+                seasonList.add(s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+			System.out.println("\n"+"=".repeat(30)+"¡ERROR EN LA BASE DE DATOS!"+"=".repeat(30)+"\n");
+        }
         return seasonList;
     }
 }
