51be83dc168d653a0d380e79cc29c2d7d1fdc9f8
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 12:52:20 -0500

    Actualización en la entidad/modelo Season.
    Se agregó el constructor y sus getters y setters.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/Season.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/Season.java
@@ -1,5 +1,33 @@
 package modelo;
 
 public class Season {
+    private int year;
+    private String url;
 
-}
+    public Season(int year, String url) {
+        this.year = year;
+        this.url = url;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+	@Override
+    public String toString() {
+        return String.valueOf(year);
+    }
+
+}   
