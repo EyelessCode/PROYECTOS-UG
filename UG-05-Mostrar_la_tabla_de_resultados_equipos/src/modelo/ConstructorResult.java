c5516360f903c1f331bb8fb5ea95e49d609ca739
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 13:00:16 -0500

    Actualización en el modelo/entidad ConstructorResult.
    Se agregó los atributos, constructor y sus getters y setters.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/ConstructorResult.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/ConstructorResult.java
@@ -1,5 +1,48 @@
 package modelo;
 
 public class ConstructorResult {
+    private String name;
+    private int wins;
+    private int totalPoints;
+    private int rank;
+    
+    public ConstructorResult(String name, int wins, int totalPoints, int rank) {
+        this.name = name;
+        this.wins = wins;
+        this.totalPoints = totalPoints;
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
 
 }
