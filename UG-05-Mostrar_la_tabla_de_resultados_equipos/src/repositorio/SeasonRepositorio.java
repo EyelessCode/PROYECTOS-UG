db49bd60681de59d0d62d8c85f3698a451da07e2
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 13:07:06 -0500

    Actualización en SeasonRepositorio.
    Se creó la url, user y password de la BD, una método lista donde la lista será retornada.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/SeasonRepositorio.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/SeasonRepositorio.java
@@ -1,5 +1,18 @@
 package repositorio;
 
-public class SeasonRepositorio {
+import java.util.ArrayList;
+import java.util.List;
 
+import modelo.Season;
+
+public class SeasonRepositorio {
+    String url="jdbc:mysql://127.0.0.1:3306/formula1";
+    String user="cristhian";
+    String password="cris03022";
+
+    public List<Season> seasonAll(){
+        List<Season> seasonList=new ArrayList<Season>();
+
+        return seasonList;
+    }
 }
