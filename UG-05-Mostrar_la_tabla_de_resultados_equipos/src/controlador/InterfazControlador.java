cdbf92b1803b9cad3033d3cd043502fcb557efc5
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 19:15:54 -0500

    Actualización en InterfazControlador.
    Se hizo el import de las librerías de JavaFX.
    Se realizó el extends de 'Application', se está aplicando como atributos a los repositorios, instanciándolos.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazControlador.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazControlador.java
@@ -1,5 +1,20 @@
 package controlador;
 
-public class InterfazControlador {
+import javafx.application.Application;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import repositorio.ConstructorResultRepositorio;
+import repositorio.DriverResultRepositorio;
+import repositorio.SeasonRepositorio;
+
+public class InterfazControlador extends Application{
+    private ConstructorResultRepositorio crr=new ConstructorResultRepositorio();
+    private DriverResultRepositorio drr=new DriverResultRepositorio();
+    private SeasonRepositorio sr=new SeasonRepositorio();
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+        
+	}
 
 }
