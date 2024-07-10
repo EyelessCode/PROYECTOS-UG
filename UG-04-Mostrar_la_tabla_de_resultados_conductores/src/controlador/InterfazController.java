583604fdb0383c08e9b0288cf52840f0483ec33b
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 21:04:59 -0500

    Actualización en InterfazController.
    Se ha creado un tableview y sus respectivas columnas, cada columna se establece el valor de cada atributo del modelo.

--- UG-04-Mostrar_la_tabla_de_resultados_conductores/src/controlador/InterfazController.java
+++ UG-04-Mostrar_la_tabla_de_resultados_conductores/src/controlador/InterfazController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import modelo.DriverResult;
 import modelo.Season;
 import repositorio.DriverResultRepositorio;
 import repositorio.SeasonRepositorio;
@@ -22,5 +26,25 @@ public class InterfazController extends Application{
         for (Season season : driverResultsList) {
             comboBox.getItems().addAll(season.getYear());
         }
+
+        //! PARA EL TABLEVIEW Y SU IMPLEMENTACIÓN A LA PRESENTACIÓN
+        TableView<DriverResult>tableView=new TableView<>();
+
+        //? IMPLEMENTACIÓN DE LA COLUMNAS - PRIMERA COLUMNA
+        TableColumn<DriverResult,String>nameColumn=new TableColumn<>("NOMBRE");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("forname"));
+        //? SEGUNDA COLUMNA
+        TableColumn<DriverResult,String>lastnameColumn=new TableColumn<>("APELLIDO");
+        lastnameColumn.setCellValueFactory(new PropertyValueFactory<>("surname"));
+        //? TERCERA COLUMNA
+        TableColumn<DriverResult,Integer>winsColumn=new TableColumn<>("VICTORIAS");
+        winsColumn.setCellValueFactory(new PropertyValueFactory<>("wins"));
+        //? CUARTA COLUMNA
+        TableColumn<DriverResult,Integer>pointsColumn=new TableColumn<>("PUNTOS TOTALES");
+        pointsColumn.setCellValueFactory(new PropertyValueFactory<>("totalPoints"));
+        //? QUINTA COLUMNA
+        TableColumn<DriverResult,Integer>rankColumn=new TableColumn<>("RANGO");
+        rankColumn.setCellValueFactory(new PropertyValueFactory<>("rank"));
+
 	}
 }
