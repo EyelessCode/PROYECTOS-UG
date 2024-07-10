01264c35e1cbd748bc29a0093f231a5c6fb14fca
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 20:45:18 -0500

    Actualización de InterfazController.
    Se hizo la extensión para 'Application', se agregó un combobox para introducir los años dentro, mediante el for-each se consigue todos los años disponibles.

--- UG-04-Mostrar_la_tabla_de_resultados_conductores/src/controlador/InterfazController.java
+++ UG-04-Mostrar_la_tabla_de_resultados_conductores/src/controlador/InterfazController.java
@@ -1,5 +1,26 @@
 package controlador;
 
-public class InterfazController {
+import java.util.List;
 
+import javafx.application.Application;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+import modelo.Season;
+import repositorio.DriverResultRepositorio;
+import repositorio.SeasonRepositorio;
+
+public class InterfazController extends Application{
+    private DriverResultRepositorio drr=new DriverResultRepositorio();
+    private SeasonRepositorio sr=new SeasonRepositorio();
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+        //! IMPLEMENTACIÓN DE LOS AÑOS AL COMBOBOX
+        ComboBox<Integer>comboBox=new ComboBox<>();
+
+        List<Season> driverResultsList=sr.yearGeneral();
+        for (Season season : driverResultsList) {
+            comboBox.getItems().addAll(season.getYear());
+        }
+	}
 }
