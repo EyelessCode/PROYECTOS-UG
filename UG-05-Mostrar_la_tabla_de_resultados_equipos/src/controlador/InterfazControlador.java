be33573a9e0f05b59ca9ef606ecf7c5d89e24f70
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-09 19:38:00 -0500

    Actualización en InterfazControlador.
    Se creó el tableview junto con cada columna y llamando a la propiedad de la entidad. Se establece cada celda.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazControlador.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazControlador.java
@@ -2,9 +2,15 @@ package controlador;
 
 import java.util.List;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
 import javafx.application.Application;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import modelo.ConstructorResult;
 import modelo.Season;
 import repositorio.ConstructorResultRepositorio;
 import repositorio.DriverResultRepositorio;
@@ -17,12 +23,29 @@ public class InterfazControlador extends Application{
 
 	@Override
 	public void start(Stage arg0) throws Exception {
+        //! PARA EL COMBOBOX Y SU IMPLEMENTACIÓN A LA PRESENTACIÓN
         ComboBox<Integer> comboBoxYear=new ComboBox<>();
         List<Season> sList=sr.seasonAll();
-
+        
         for (Season season : sList) {
             comboBoxYear.getItems().add(season.getYear());
         }
-	}
+        
+        //! PARA EL TABLEVIEW Y SU IMPLEMENTACIÓN A LA PRESENTACIÓN
+        TableView<ConstructorResult> constructorTableView=new TableView<>();
+
+        //? IMPLEMENTACIÓN DE LA COLUMNAS - PRIMERA COLUMNA
+        TableColumn<ConstructorResult,String> nameColumn=new TableColumn<>("EQUIPO");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        //? SEGUNDA COLUMNA
+        TableColumn<ConstructorResult,Integer>winsColumn=new TableColumn<>("VICTORIAS");
+        winsColumn.setCellValueFactory(new PropertyValueFactory<>("wins"));
+        //? TERCERA COLUMNA
+        TableColumn<ConstructorResult,Integer>pointsColumn=new TableColumn<>("PUNTOS TOTALES");
+        pointsColumn.setCellValueFactory(new PropertyValueFactory<>("totalPoints"));
+        //? CUARTA COLUMNA
+        TableColumn<ConstructorResult,Integer>rankColumn=new TableColumn<>("RANGO");
+        rankColumn.setCellValueFactory(new PropertyValueFactory<>("rank"));
+    }
 
 }
