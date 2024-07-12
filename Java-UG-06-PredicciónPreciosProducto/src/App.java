81eb6cee35f795c631b869f10e28d473a1b69ecf
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-11 23:04:18 -0500

    Actualización en App.
    Se agregó una subclase dentro de la clase o main, se un constructor y dos métodos, estos retornan a las variables.

--- Java-UG-06-PredicciónPreciosProducto/src/App.java
+++ Java-UG-06-PredicciónPreciosProducto/src/App.java
@@ -1,5 +1,21 @@
 public class App {
+    static class PrediccionPrecio {
+        public int dia;
+        public double precio;
+        
+        public PrediccionPrecio(int dia,double precio){
+            this.dia=dia;
+            this.precio=precio;
+        }
+    }
+
+    public static double inteLineal(double x0, double y0, double x1, double y1, double x) {
+        return y0 + (y1 - y0) * ((x - x0) / (x1 - x0));
+    }
+
+    public static double extrapolateLinear(double x0, double y0, double x1, double y1, double x) {
+        return inteLineal(x0, y0, x1, y1, x);
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
     }
 }
