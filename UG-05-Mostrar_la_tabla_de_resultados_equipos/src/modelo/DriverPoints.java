1fd0b4b63760dc7c8ee1566d34e98a813045f7de
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-14 17:00:32 -0500

    Actualización en DriverPoints.
    Se creó y agregó los atributos adecuados, el constructor y los getters y setters.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/DriverPoints.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/DriverPoints.java
@@ -1,5 +1,28 @@
 package modelo;
 
 public class DriverPoints {
+    private String driverName;
+    private int totalPoints;
+    
+    public DriverPoints(String driverName, int totalPoints) {
+        this.driverName = driverName;
+        this.totalPoints = totalPoints;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
 
 }
