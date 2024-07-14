cc8643f4683f7048ed46d55e05c9ff6114e514e9
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-14 17:11:18 -0500

    Actualización en DriverPointsRepositorio.
    Se agregó el url, user y password para el acceso al usuario en la BD. Se creó una lista para guardar los listados y que el método lista retorne los listados.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/DriverPointsRepositorio.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/DriverPointsRepositorio.java
@@ -1,5 +1,18 @@
 package repositorio;
 
-public class DriverPointsRepositorio {
+import java.util.ArrayList;
+import java.util.List;
 
+import modelo.DriverPoints;
+
+public class DriverPointsRepositorio {
+    String url="jdbc:postgresql://localhost:5432/ug_formula01";
+    String user="cristhian";
+    String password="cris03022";
+
+    public List<DriverPoints> totalPointsOrder(){
+        List<DriverPoints> listDriverPoints=new ArrayList<DriverPoints>();
+
+        return listDriverPoints;
+    }
 }
