bd55b1d71ee528dcbb94bdccf3a94a4d91b71935
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-14 17:54:44 -0500

    Actualización en ConstructorPoints.
    Se agregó los atributos, constructor y getters y setters para el listado del futuro repositorio.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/ConstructorPoints.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/modelo/ConstructorPoints.java
@@ -1,5 +1,28 @@
 package modelo;
 
 public class ConstructorPoints {
+    private String name;
+    private int totalPoints;
     
+    public ConstructorPoints(String name, int totalPoints) {
+        this.name = name;
+        this.totalPoints = totalPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
 }
