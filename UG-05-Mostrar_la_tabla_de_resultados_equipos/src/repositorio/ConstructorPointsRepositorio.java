f3f362063ccc9d067bdff2bd21ed67cd5b4cc837
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-14 18:42:03 -0500

    Actualización en ConstructorPointsRepositorio.
    Se agregó un try-catch para el intento de la conexión a la base de datos, el connection realiza la conexión con el url, user y password, luego se crea la consulta, se realiza la declaración, la declaración se almacena luego se establece gracias a resultset y retorna.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/ConstructorPointsRepositorio.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/ConstructorPointsRepositorio.java
@@ -1,5 +1,9 @@
 package repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +17,30 @@ public class ConstructorPointsRepositorio {
     public List<ConstructorPoints> orderTotalPoints(){
         List<ConstructorPoints> listConstructorPoints=new ArrayList<ConstructorPoints>();
 
+        try {
+            Connection cnt=DriverManager.getConnection(url, user, password);
+
+            String sql = "SELECT\n" +
+             "    co.name,\n" +
+             "    SUM(res.points) AS total_points\n" +
+             "FROM\n" +
+             "    results res\n" +
+             "JOIN\n" +
+             "    races r ON res.race_id = r.race_id\n" +
+             "JOIN\n" +
+             "    constructors co ON res.constructor_id = co.constructor_id\n" +
+             "GROUP BY\n" +
+             "    co.constructor_id, co.name\n" +
+             "ORDER BY\n" +
+             "    total_points DESC;";
+
+            Statement st=cnt.createStatement();
+            ResultSet rs=st.executeQuery(sql);
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return listConstructorPoints;
     }
 }
