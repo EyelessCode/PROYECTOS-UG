9692f0fdde638be34fe9bbcac1d9328c984d007c
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-14 18:02:11 -0500

    Actualización en ConstructorPointsRepositorio.
    Se agregó la url, user y password para el acceso al BD, se creó una lista para agregar y dentro del método lista un retorno de los listados.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/ConstructorPointsRepositorio.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/ConstructorPointsRepositorio.java
@@ -1,5 +1,18 @@
 package repositorio;
 
-public class ConstructorPointsRepositorio {
+import java.util.ArrayList;
+import java.util.List;
 
+import modelo.ConstructorPoints;
+
+public class ConstructorPointsRepositorio {
+    String url="jdbc:postgresql://localhost:5432/ug_formula01";
+    String user="cristhian";
+    String password="cris03022";
+
+    public List<ConstructorPoints> orderTotalPoints(){
+        List<ConstructorPoints> listConstructorPoints=new ArrayList<ConstructorPoints>();
+
+        return listConstructorPoints;
+    }
 }
