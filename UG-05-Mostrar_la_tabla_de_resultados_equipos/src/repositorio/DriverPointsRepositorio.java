ab4c690d0131aa7cff6bdcfd2e42dc390399a54a
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-14 17:18:00 -0500

    Actualización en DriverPointsRepositorio.
    Se creó el conector que realiza el acceso al url, user y password. Se creó la declaración, y el 'resultset'. Todo esto para ejecutar el 'query' que es la consulta. Esto permitirá realizar la consulta mediante el VSCode sin necesidad de estar en la BD.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/DriverPointsRepositorio.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/repositorio/DriverPointsRepositorio.java
@@ -1,5 +1,9 @@
 package repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +17,32 @@ public class DriverPointsRepositorio {
     public List<DriverPoints> totalPointsOrder(){
         List<DriverPoints> listDriverPoints=new ArrayList<DriverPoints>();
 
+        try {
+            Connection cnt=DriverManager.getConnection(url, user, password);
+            
+            String sql= "SELECT\n"+
+            "   d.forename,\n"+
+            "   d.surname,\n"+
+            "SUM(res.points) AS total_points\n"+
+            "FROM\n"+
+            "   results res\n"+
+            "JOIN\n"+
+            "   races r ON res.race_id = r.race_id\n"+
+            "JOIN\n"+
+            "   drivers d ON res.driver_id = d.driver_id\n"+
+            "GROUP BY\n"+
+            "   d.driver_id, d.forename, d.surname\n"+
+            "ORDER BY\n"+
+            "   total_points DESC;";
+
+            Statement st=cnt.createStatement();
+            ResultSet rs=st.executeQuery(sql);
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+			System.out.println("\n"+"=".repeat(30)+"¡ERROR EN LA BASE DE DATOS!"+"=".repeat(30)+"\n");
+        }
         return listDriverPoints;
     }
 }
