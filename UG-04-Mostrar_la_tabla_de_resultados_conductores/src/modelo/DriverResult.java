0ec8ab72ee232b0af8ed7ffb6e4214edc0c5a625
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-15 19:35:46 -0500

    Ligeros cambios en el modelo/entidad DriverResult.
    Se modificó toda la estructura del modelo/entidad, dando como resultado una estructura actualizada.

--- UG-04-Mostrar_la_tabla_de_resultados_conductores/src/modelo/DriverResult.java
+++ UG-04-Mostrar_la_tabla_de_resultados_conductores/src/modelo/DriverResult.java
@@ -14,31 +14,37 @@ public class DriverResult {
 		this.totalPoints = totalPoints;
 		this.rank = rank;
 	}
+
 	public String getDriverName() {
 		return driverName;
 	}
+
 	public void setDriverName(String driverName) {
 		this.driverName = driverName;
 	}
+
 	public int getWins() {
 		return wins;
 	}
+
 	public void setWins(int wins) {
 		this.wins = wins;
 	}
+
 	public int getTotalPoints() {
 		return totalPoints;
 	}
+
 	public void setTotalPoints(int totalPoints) {
 		this.totalPoints = totalPoints;
 	}
+
 	public int getRank() {
 		return rank;
 	}
+
 	public void setRank(int rank) {
 		this.rank = rank;
 	}
-	
-	
 	
 }
