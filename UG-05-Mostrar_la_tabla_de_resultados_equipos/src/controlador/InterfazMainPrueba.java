c2d483046e6f04b9e683d8e55e46b5509f12f3f5
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-17 18:34:03 -0500

    Actualización en InterfazMainPrueba y InterfazVentana.
    En InterfazMainPrueba se creó el contenido para la ejecución de la ventana principal, donde dentro de esta clase interfaz se crea un método para hacer una instanciación de la otra interfaz que sería la ventana secundaria.
    InterfazVentana en se creó un método para la ejecución de la segunda ventana. Se borró la extensión Application.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazMainPrueba.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazMainPrueba.java
@@ -1,14 +1,31 @@
 package controlador;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class InterfazMainPrueba extends Application{
     private InterfazVentana iv;
-    
+
 	@Override
 	public void start(Stage arg0) throws Exception {
+        Button btn=new Button("ABRIR VENTANA EMERGENTE");
+        btn.setOnAction(ev->{
+            abrirVentana();
+            StackPane root=new StackPane();
+            root.getChildren().add(btn);
 
+            Scene ventana=new Scene(root,300,200);
+            arg0.setTitle("VENTANA PRINCIPAL");
+            arg0.setScene(ventana);
+            arg0.show();
+        });
     }
 
+    private void abrirVentana(){
+        iv=new InterfazVentana();
+        iv.display();
+    }
 }
--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazVentana.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazVentana.java
@@ -1,13 +1,24 @@
 package controlador;
 
-import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class InterfazVentana extends Application{
+public class InterfazVentana {
 
-    @Override
-    public void start(Stage arg0) throws Exception {
-        
+    public void display(){
+        Stage nuevaVentana=new Stage();
+        StackPane secundaria=new StackPane();
+        Label l=new Label("HOLA DESDE LA NUEVA VENTANA");
+        secundaria.getChildren().add(l);
+
+        Scene segunVentana=new Scene(secundaria,230,100);
+
+        nuevaVentana.setTitle("NUEVA VENTANITA");
+        nuevaVentana.setScene(segunVentana);;
+        nuevaVentana.show();
     }
+
 
 }
