5c56d3a3bfa7c8ea9d458107a7bb2fd8488afa6a
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-17 18:05:10 -0500

    Creación de una nueva clase interfaz y eliminación del contenido de InterfazMainPrueba.
    Se creó una nueva interfaz, dónde se llamará a la ventana emergente que sólo funcionará y ejecutará si la otra interfaz es presionado el botón.
    Se borró el contenido de la interfaz InterfazMainPrueba para poder hacer la prueba de la ventana principal.

---
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/InterfazVentana.java
@@ -0,0 +1,13 @@
+package controlador;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InterfazVentana extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+    }
+
+}
