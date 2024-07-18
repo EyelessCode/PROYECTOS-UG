cf8daa606f1a5cbf0459037f13e0211d10ba4830
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-18 18:31:46 -0500

    Ligeros cambios en VentanaPrincipal y creación de un nuevo package.
    Se agregó la clase 'Image' para aplicar una imagen, se especificó la ruta y se creó un package para imagenes, por lo que se agregó una imagen dentro de ello.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/vista/VentanaPrincipal.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/vista/VentanaPrincipal.java
@@ -1,13 +1,14 @@
 package vista;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class VentanaPrincipal extends Application{
 
     @Override
     public void start(Stage arg0) throws Exception {
-        
+        Image imagen=new Image(getClass().getResourceAsStream("C:\\Users\\Cristhian\\Documents\\GitHub\\PROYECTOS-UG\\UG-05-Mostrar_la_tabla_de_resultados_equipos\\src\\imagenes○\\formula1.jpg"));
     }
     
 }
