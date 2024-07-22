52257eb57b8672a2502d0e24ff9a8f3a0677d027
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-07-21 21:25:50 -0500

    Actualización en GraphicsDriversController.
    Se aplicó una etiqueta, con su tamaña y su fuente, se está agregando los ejes para el gráfico de barra.

--- UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/GraphicsDriversController.java
+++ UG-05-Mostrar_la_tabla_de_resultados_equipos/src/controlador/GraphicsDriversController.java
@@ -1,5 +1,24 @@
 package controlador;
 
-public class GraphicsDriversController {
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import repositorio.DriverPointsRepositorio;
 
+public class GraphicsDriversController {
+    private DriverPointsRepositorio dpr=new DriverPointsRepositorio();
+
+    public void ventanaEmergente(){
+        Font fuente=new Font("Gill Sans Ultra Bold", 36);
+        Label etiqueGrafico=new Label("GRÁFICO DE BARRAS DE CONDUCTORES");
+        etiqueGrafico.setFont(fuente);
+
+        CategoryAxis x=new CategoryAxis();
+        x.setLabel("NOMBRES/APELLIDOS");
+
+        NumberAxis y=new NumberAxis();
+        y.setLabel("PUNTAJE TOTAL");
+
+    }
 }
