326881509cc3007974511feb0cecf73c28b8202c
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-11-25 21:57:55 -0500

    Actualización gigante.
    Se incorporaron nuevas cosas, también se corrigieron. y se agregaron.

---
+++ PROYECTOS-UG-APP_WEBS/JS-03-PLANESVIVIENDA/js/logicaCalculo.js
@@ -0,0 +1,12 @@
+import {planosList} from './DDBB.js';
+
+const txtCuotaEntrada=document.getElementById('txtCuotaEntrada');
+const txtPrecio = document.getElementById("txtPrecio");
+
+
+export function calculoCuotas(tasaInteresCuotaEntrada,precioVivienda){
+    let cuotaEntrada=precioVivienda*tasaInteresCuotaEntrada;
+    let planSeleccionado=planosList.
+    
+    return cuotaEntrada;
+};
