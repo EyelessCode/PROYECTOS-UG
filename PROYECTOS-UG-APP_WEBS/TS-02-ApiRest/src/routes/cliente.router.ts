3da2dc9df3fa3d1921344e8b167b82acb2de81f5
Cristhian Guedes <cristhianjosue03@outlook.com>
2024-12-27 17:06:54 -0500

    Update cliente.router.ts
    Se agregó contenido.

---
+++ PROYECTOS-UG-APP_WEBS/TS-02-ApiRest/src/routes/cliente.router.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { ClienteController } from "../controllers/cliente.controller";
+
+const router=Router();
+const controller=new ClienteController();
+
+router.get('/clientes',controller.obtenerTodo);
+router.get('/clientes\:codigo',controller.obtenerCodigo);
+router.post('/clientes/',controller.crear);
+router.put('/clientes/:codigo',controller.actualizar);
+router.delete('/clientes/:codigo',controller.eliminar);
+
+export default router;
