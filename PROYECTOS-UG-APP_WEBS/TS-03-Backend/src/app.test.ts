9f686028e736f01c1acaa6d75b9ceb5e79877b8e
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-09 18:54:26 -0500

    Transpilación

--- PROYECTOS-UG-APP_WEBS/TS-03-Backend/src/app.test.ts
+++ PROYECTOS-UG-APP_WEBS/TS-03-Backend/src/app.test.ts
@@ -7,16 +7,22 @@ let signoTest:ISignoVital={
 }
 
 let repository:SignoVital=new SignoVital();
+
 repository.getAll().forEach((signoEach)=>console.log(
     `código: ${signoEach.codigo} - descripción: ${signoEach.descripcion}`
 ))
+
 repository.createSigno(signoTest)
+
 repository.getAll().forEach((signoEach)=>console.log(
     `Llamando de nuevo, código: ${signoEach.codigo} - descripción: ${signoEach.descripcion}`
 ))
+
 let getOne=repository.getOneSigno(10)
 console.log(`Código: ${getOne?.codigo} Descripcion: ${getOne?.descripcion}`);
+
 repository.deleteSigno(10)
+
 repository.getAll().forEach((signoEach)=>console.log(
     `Llamando por 2da vez, código: ${signoEach.codigo} - descripción: ${signoEach.descripcion}`
 ))
