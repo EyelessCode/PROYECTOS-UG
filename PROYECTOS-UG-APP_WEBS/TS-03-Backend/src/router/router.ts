01d7ff7a8d114d8348aee4663dc772cbedb4d814
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-09 19:26:56 -0500

    Update router.ts

--- PROYECTOS-UG-APP_WEBS/TS-03-Backend/src/router/router.ts
+++ PROYECTOS-UG-APP_WEBS/TS-03-Backend/src/router/router.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { SignoVitalController } from "../controller/signoVitalController";
 
 const router=Router()
-router.get('/signoVital')
-router.get('/signoVital/:id')
-router.post('/signoVital')
-router.put('/signoVital/:id')
-router.delete('/signoVital/:id')
+const controller=new SignoVitalController()
+
+router.get('/signoVital',controller.controllerGetAll)
+router.get('/signoVital/:id',controller.controllerGetOne)
+router.post('/signoVital',controller.controllerCreate)
+router.put('/signoVital/:id',controller.controllerUpdate)
+router.delete('/signoVital/:id',controller.controllerDelete)
 
 
 export default router;
