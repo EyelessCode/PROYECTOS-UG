0b99f958072a97e9bed7dbb411c262fd8d0e521c
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-01-09 19:27:00 -0500

    transpilation

--- PROYECTOS-UG-APP_WEBS/TS-03-Backend/dist/router/router.js
+++ PROYECTOS-UG-APP_WEBS/TS-03-Backend/dist/router/router.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const signoVitalController_1 = require("../controller/signoVitalController");
 const router = (0, express_1.Router)();
-router.get('/signoVital');
-router.get('/signoVital/:id');
-router.post('/signoVital');
-router.put('/signoVital/:id');
-router.delete('/signoVital/:id');
+const controller = new signoVitalController_1.SignoVitalController();
+router.get('/signoVital', controller.controllerGetAll);
+router.get('/signoVital/:id', controller.controllerGetOne);
+router.post('/signoVital', controller.controllerCreate);
+router.put('/signoVital/:id', controller.controllerUpdate);
+router.delete('/signoVital/:id', controller.controllerDelete);
 exports.default = router;
 //# sourceMappingURL=router.js.map
