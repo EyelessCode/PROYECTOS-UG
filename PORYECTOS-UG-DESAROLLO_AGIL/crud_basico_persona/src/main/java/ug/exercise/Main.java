c7932681d954b36a4f5859c2c50aa1a8928de05c
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-06-16 09:51:43 -0500

    Update Main.java

--- PORYECTOS-UG-DESAROLLO_AGIL/crud_basico_persona/src/main/java/ug/exercise/Main.java
+++ PORYECTOS-UG-DESAROLLO_AGIL/crud_basico_persona/src/main/java/ug/exercise/Main.java
@@ -1,7 +1,11 @@
 package ug.exercise;
 
+import ug.exercise.ui.InterfaceConsole;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
+        InterfaceConsole ui=new InterfaceConsole();
+        ui.inizializateMenu();
     }
 }
